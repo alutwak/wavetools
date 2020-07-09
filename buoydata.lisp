86151a29f3ce573c7540c9c4b9d84d05aaae166d
Ayal Lutwak <worm60@gmail.com>
2020-07-09 00:48:58 -0700

    Switched to using streams for data parsing

--- buoydata.lisp
+++ buoydata.lisp
@@ -1,7 +1,7 @@
 
 (declaim (optimize (debug 3)))
 
-(ql:quickload '(:dexador :plump :lquery :lparallel :uiop :str :cl-ppcre) :silent t)
+(ql:quickload '(:dexador :plump :lquery :lparallel :uiop :str :cl-ppcre :bt-semaphore) :silent t)
 
 ;;(load (compile-file "wavespectrum.lisp"))
 
@@ -44,6 +44,9 @@ is only really useful when multiple hits on the data are done within a few hours
 (defvar *hist-path* "hist/"
   "Path for historical data cache.")
 
+(defun this-year ()
+  (nth-value 5 (get-decoded-time)))
+
 (defun get-hist-data-path (station-id &optional month-or-year)
   (str:concat (get-station-cache-path station-id) *hist-path* month-or-year))
 
@@ -82,16 +85,36 @@ is only really useful when multiple hits on the data are done within a few hours
 ;; ----------------------------------- Raw Data (in string format) -------------------------------
 
 (defstruct (raw-data (:constructor make-raw-data (c a1 a2 r1 r2)))
-  (c nil :read-only t)
-  (a1 nil :read-only t)
-  (a2 nil :read-only t)
-  (r1 nil :read-only t)
-  (r2 nil :read-only t))
+  (c nil :type stream :read-only t)
+  (a1 nil :type stream :read-only t)
+  (a2 nil :type stream :read-only t)
+  (r1 nil :type stream :read-only t)
+  (r2 nil :type stream :read-only t))
+
+(defun raw-data->list (rd)
+  (list (raw-data-c rd) (raw-data-a1 rd) (raw-data-a2 rd) (raw-data-r1 rd) (raw-data-r2 rd)))
+
+(defun raw-data-reset (rd)
+  (mapc (lambda (getter)
+          (file-position (funcall getter rd) 0))
+        (list #'raw-data-c #'raw-data-a1 #'raw-data-a2 #'raw-data-r1 #'raw-data-r2)))
+
+(defun raw-data-read-line (rd)
+  "Reads lines from all raw data streams and returns them"
+  (values-list
+   (mapcar
+    (lambda (strm)
+      (read-line strm))
+    (raw-data->list rd))))
+
+(defun eof-p (stream)
+  (not (peek-char nil stream nil nil)))
+
+(defun raw-data-eof-p (rd)
+  (every #'eof-p (raw-data->list rd)))
 
 (defvar *date-scan* (ppcre:create-scanner "(\\d{4}) (\\d{2}) (\\d{2}) (\\d{2}) (\\d{2})")
   "Regex for retrieving the date from a buoy data row")
-
-
 
 (defun parse-date (row-str)
   (ppcre:register-groups-bind
@@ -100,10 +123,9 @@ is only really useful when multiple hits on the data are done within a few hours
     row-str)
    (encode-universal-time 0 min hour day month year)))
 
-
 ;;; Real Time Data
 
-(defun parse-rtd (raw)
+(defun parse-rtd (rd)
   "Parses real-time data formatted strings into a list of spectral-points. 
 
 Each parameter comes in a separate file (which are provided to parse-rtd as strings). Each line of each file starts with
@@ -111,15 +133,15 @@ a 5-number date, and the values of the parameter for each frequency are given as
 The separation frequency is given in the spec file (which defines the 'c' parameter)."
   (let ((fsep-scan (ppcre:create-scanner "([0-9.]+)"))                                     ; regex for getting the fsep
         (stat-freq-scan (ppcre:create-scanner "([0-9.]+) \\(([0-9.]+)\\)")))           ; regex for getting the stat and freq
-    (labels ((parse-row (row date? sep-freq?)
-               "Parses a single row from one of the parameter files.
+    (labels ((parse-line (line c-data?)
+               "Parses a single line from one of the parameter files.
 
-                If date? or sep-freq? are non-nil, they will be captured from the row"
-               (let ((date (if date? (parse-date row) nil))
-                     (fsep (if sep-freq?
+                If c-data? is non-nil, the freqs and fsep values will be captured"
+               (let ((date (if c-data? (parse-date line) nil))
+                     (fsep (if c-data?
                                (ppcre:register-groups-bind ((#'read-from-string fsep))
                                    (fsep-scan
-                                    row
+                                    line
                                     :start 17)
                                  fsep)
                                nil))
@@ -127,45 +149,55 @@ The separation frequency is given in the spec file (which defines the 'c' parame
                      (freqs))
                  (ppcre:do-register-groups ((#'read-from-string stat freq))
                      (stat-freq-scan
-                      row
+                      line
                       nil
-                      :start (if sep-freq? 23 17))
+                      :start (if c-data? 23 17))
                    (progn
                      (setq data (cons stat data))
                      (setq freqs (cons freq freqs))))
                  (list (reverse data) (reverse freqs) date fsep)))
-             (parse-all-rows (l-spec l-a1 l-a2 l-r1 l-r2)
-               "Recursively parses all rows (after they have been split into lines). This assumes that the header has been
-                skipped already."
-               (let* ((c-f-d-fsep (parse-row (car l-spec) t t))
-                      (a1 (car (parse-row (car l-a1) nil nil)))
-                      (a2 (car (parse-row (car l-a2) nil nil)))
-                      (r1 (car (parse-row (car l-r1) nil nil)))
-                      (r2 (car (parse-row (car l-r2) nil nil)))
-                      (point (make-spectral-point
-                              (nth 2 c-f-d-fsep)                   ; time stamp
-                              (map 'vector #'make-spect-params     ; params
-                                   (nth 1 c-f-d-fsep)      ; f
-                                   (car c-f-d-fsep)        ; c
-                                   a1                      
-                                   a2
-                                   r1
-                                   r2)
-                              (nth 3 c-f-d-fsep))))                ; fsep
-                 (if (every #'cdr (list l-spec l-a1 l-a2 l-r1 l-r2))
-                     (cons point (parse-all-rows (cdr l-spec) (cdr l-a1) (cdr l-a2) (cdr l-r1) (cdr l-r2)))
-                     `(,point)))))
 
-      ;; Split each stat string by line
-      ;; Parse all rows, skipping the header line
-      (reverse (parse-all-rows
-                (cdr (str:lines (raw-data-c raw)))
-                (cdr (str:lines (raw-data-a1 raw)))
-                (cdr (str:lines (raw-data-a2 raw)))
-                (cdr (str:lines (raw-data-r1 raw)))
-                (cdr (str:lines (raw-data-r2 raw))))))))
+             (parse-all-lines (c-line a1-line a2-line r1-line r2-line)
+               "Parses lines for all raw data streams"
+               (let* ((c-f-d-fsep (parse-line c-line t))
+                      (a1 (car (parse-line a1-line nil)))
+                      (a2 (car (parse-line a2-line nil)))
+                      (r1 (car (parse-line r1-line nil)))
+                      (r2 (car (parse-line r2-line nil))))
+                 (make-spectral-point
+                  (nth 2 c-f-d-fsep)               ; time stamp
+                  (map 'vector #'make-spect-params ; params
+                       (nth 1 c-f-d-fsep)          ; f
+                       (car c-f-d-fsep)            ; c
+                       a1                      
+                       a2
+                       r1
+                       r2)
+                  (nth 3 c-f-d-fsep)))) ; fsep
 
-(defun download-station-rtd (station-id)
+             (parse-next-entry ()
+               "Recursively reads lines from the raw data streams and parses them to create spectral points"
+               (if (raw-data-eof-p rd)
+                   nil
+                   (cons
+                    (multiple-value-bind
+                     (c a1 a2 r1 r2)
+                     (raw-data-read-line rd)
+                     (parse-all-lines c a1 a2 r1 r2))
+                    (parse-next-entry)))))
+
+      (raw-data-read-line rd) ;; Discard header
+      (reverse (parse-next-entry)))))
+
+(defun cache-station-rtd (station-id raw-c raw-a1 raw-a2 raw-r1 raw-r2)
+  "Saves data to the real-time data cache for the station with station-id."
+  (let ((rtd-data-path (ensure-rtd-data-path-exists station-id)))
+    (mapcar (lambda (data name)
+              (str:to-file (str:concat rtd-data-path name) data))
+            (list raw-c raw-a1 raw-a2 raw-r1 raw-r2)
+            '("c" "a1" "a2" "r1" "r2"))))
+
+(defun download-station-rtd (station-id &optional cache-data)
   "Downloads the real-time data for the specified station ID"
   (flet ((get-data (path)
            (dex:get (format nil "https://www.ndbc.noaa.gov/data/realtime2/~D.~A" station-id path))))
@@ -174,24 +206,39 @@ The separation frequency is given in the spec file (which defines the 'c' parame
           (a2-data (get-data "swdir2"))
           (r1-data (get-data "swr1"))
           (r2-data (get-data "swr2")))
-      (make-raw-data c-data a1-data a2-data r1-data r2-data))))
 
-(defun cache-station-rtd (station-id raw-data)
-  "Saves data to the real-time data cache for the station with station-id."
-  (let ((rtd-data-path (ensure-rtd-data-path-exists station-id)))
-    (mapcar (lambda (getter name)
-              (str:to-file (str:concat rtd-data-path name) (funcall getter raw-data)))
-            (list #'raw-data-c #'raw-data-a1 #'raw-data-a2 #'raw-data-r1 #'raw-data-r2)
-            '("c" "a1" "a2" "r1" "r2"))))
+      (when cache-data
+        ;; Do it in a thread so we don't hang this up
+        (bt:make-thread
+         (lambda ()
+           (cache-station-rtd station-id c-data a1-data a2-data r1-data r2-data))))
 
-(defun read-rtd-cache (station-id)
-  (let* ((cache-path (get-rtd-data-path station-id)) 
-         (c-data (str:from-file (str:concat cache-path "c")))
-         (a1-data (str:from-file (str:concat cache-path "a1")))
-         (a2-data (str:from-file (str:concat cache-path "a2")))
-         (r1-data (str:from-file (str:concat cache-path "r1")))
-         (r2-data (str:from-file (str:concat cache-path "r2"))))
-    (make-raw-data c-data a1-data a2-data r1-data r2-data)))
+      (parse-rtd (make-raw-data
+                  (make-string-input-stream c-data)
+                  (make-string-input-stream a1-data)
+                  (make-string-input-stream a2-data)
+                  (make-string-input-stream r1-data)
+                  (make-string-input-stream r2-data))))))
+
+(defun read-station-rtd-cache (station-id)
+  (let* ((cache-path (get-rtd-data-path station-id))
+         (rd (make-raw-data
+              (open (str:concat cache-path "c"))
+              (open (str:concat cache-path "a1"))
+              (open (str:concat cache-path "a2"))
+              (open (str:concat cache-path "r1"))
+              (open (str:concat cache-path "r2")))))
+    (prog1
+        (parse-rtd rd)
+      (mapc #'close (raw-data->list rd)))))
+
+(defun get-rtd-data-date (raw-data)
+  (handler-case
+      (with-open-file (f (str:concat (get-rtd-data-path station-id) "c"))
+        (read-line f)
+        (let ((line1 (read-line f)))
+          (parse-date line1)))
+    (file-does-not-exist () nil)))
 
 (defun get-rtd-cache-date (station-id)
   (handler-case
@@ -203,7 +250,7 @@ The separation frequency is given in the spec file (which defines the 'c' parame
 
 ;;; Historical Data
 
-(defun parse-hist (raw)
+(defun parse-hist (rd)
   "Parses historical data formatted strings into a list of spectral-points. 
 
 Each parameter comes in a separate file (which are provided to parse-rtd as strings). Each line of each file starts with
@@ -211,68 +258,62 @@ a 5-number date, and the values of the parameter for each frequency are given as
 The separation frequency is given in the spec file (which defines the 'c' parameter)."
   (let ((stat-freq-scan (ppcre:create-scanner "\\s+([0-9.]+)")) ; regex for getting the stat and freq
         (freqs))                                                ; register for frquencies 
-    (labels ((parse-row (row date?)
-               "Parses a single row from one of the parameter files.
+    (labels ((parse-line (line c-data?)
+               "Parses a single line from one of the parameter files.
 
-                If date? or sep-freq? are non-nil, they will be captured from the row"
-               (let ((date (if date? (parse-date row) nil))
+                If c-data? is non-nil, the date will be captured"
+               (let ((date (if c-data? (parse-date line) nil))
                      (data))
                  (ppcre:do-register-groups ((#'read-from-string stat))
                      (stat-freq-scan
-                      row
+                      line
                       nil
                       :start 16)
-                   (progn
-                     (setq data (cons (float stat) data))))
+                   (setq data (cons (float stat) data)))
                  (list (reverse data) date)))
-             (parse-all-rows (l-spec l-a1 l-a2 l-r1 l-r2)
-               "Recursively parses all rows (after they have been split into lines). This assumes that the header has been
-                skipped already."
-               (let* ((c-d (parse-row (car l-spec) t))
-                      (a1 (car (parse-row (car l-a1) nil)))
-                      (a2 (car (parse-row (car l-a2) nil)))
-                      (r1 (car (parse-row (car l-r1) nil)))
-                      (r2 (car (parse-row (car l-r2) nil)))
-                      (point (make-spectral-point
-                              (cadr c-d)                       ; time stamp
-                              (map 'vector #'make-spect-params ; params
-                                   freqs
-                                   (car c-d) ; c
-                                   a1                      
-                                   a2
-                                   r1
-                                   r2))))
-                 (if (every #'cdr (list l-spec l-a1 l-a2 l-r1 l-r2))
-                     (cons point (parse-all-rows (cdr l-spec) (cdr l-a1) (cdr l-a2) (cdr l-r1) (cdr l-r2)))
-                     `(,point)))))
+             
+             (parse-all-lines (c-line a1-line a2-line r1-line r2-line)
+               "Parses lines for all raw data streams"
+               (let* ((c-d (parse-line c-line t))
+                      (a1 (car (parse-line a1-line nil)))
+                      (a2 (car (parse-line a2-line nil)))
+                      (r1 (car (parse-line r1-line nil)))
+                      (r2 (car (parse-line r2-line nil))))
+                 (make-spectral-point
+                  (cadr c-d)                       ; time stamp
+                  (map 'vector #'make-spect-params ; params
+                       freqs
+                       (car c-d)        ; c
+                       a1                      
+                       a2
+                       r1
+                       r2))))
 
-      (let ((c-lines (str:lines (raw-data-c raw)))
-            (a1-lines (str:lines (raw-data-a1 raw)))
-            (a2-lines (str:lines (raw-data-a2 raw)))
-            (r1-lines (str:lines (raw-data-r1 raw)))
-            (r2-lines (str:lines (raw-data-r2 raw))))
+             (parse-next-entry ()
+               "Recursively reads lines from the raw data streams and parses them to create spectral points"
+               (if (raw-data-eof-p rd)
+                   nil
+                   (cons
+                    (multiple-value-bind
+                          (c a1 a2 r1 r2)
+                        (raw-data-read-line rd)
+                      (parse-all-lines c a1 a2 r1 r2))
+                    (parse-next-entry)))))
 
-        ;; Get the frequencies
-        (ppcre:do-register-groups ((#'read-from-string freq))
-            (stat-freq-scan
-             (car c-lines)
-             nil
-             :start 16)
-          (setq freqs (cons freq freqs)))
+      ;; Get the frequencies
+      (ppcre:do-register-groups ((#'read-from-string freq))
+          (stat-freq-scan
+           (raw-data-read-line rd)      ; Returns first line of c data and throws away the other data files' lines
+           nil
+           :start 16)
+        (setq freqs (cons freq freqs)))
 
-        (setq freqs (reverse freqs)) ;; Reverse them to put them in order
-        
-        ;; Split each stat string by line
-        ;; Parse all rows, skipping the header line
-        (reverse (parse-all-rows
-                  (cdr c-lines)
-                  (cdr a1-lines)
-                  (cdr a2-lines)
-                  (cdr r1-lines)
-                  (cdr r2-lines)))))))
+      ;; Reverse them to put them in order      
+      (setq freqs (reverse freqs))
 
-(defun this-year ()
-  (nth-value 5 (get-decoded-time)))
+      ;; Return parsed data
+      (reverse (parse-next-entry)))))
+
 
 (defun download-station-hist (station-id year-or-month)
   (labels ((get-year-data (char-key path)
@@ -301,4 +342,11 @@ The separation frequency is given in the spec file (which defines the 'c' parame
           (a2-data (get-data #\i "swdir2"))
           (r1-data (get-data #\j "swr1"))
           (r2-data (get-data #\k "swr2")))
-      (make-raw-data c-data a1-data a2-data r1-data r2-data))))
+
+      (parse-hist (make-raw-data
+                   (make-string-input-stream c-data)
+                   (make-string-input-stream a1-data)
+                   (make-string-input-stream a2-data)
+                   (make-string-input-stream r1-data)
+                   (make-string-input-stream r2-data))))))
+
