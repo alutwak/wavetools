d82ee30e6fefec38d4d6b06b4d6e3f57da6152d5
Ayal Lutwak <worm60@gmail.com>
2020-07-11 23:27:00 -0700

    Clean up and documentation

--- buoydata.lisp
+++ buoydata.lisp
@@ -24,7 +24,7 @@
   (format nil *station-cache* station-id))
 
 (defun ensure-station-cache-exists (station-id)
-  "Ensures that the cache directory exists for the station and returns the path."
+  "Returns the path of the cache for the given station-id and creates the directory path if it doesn't exist yet"
   (let ((cache-path (get-station-cache-path station-id)))
     (ensure-directories-exist cache-path)
     cache-path))
@@ -35,9 +35,11 @@ In general, the historical data will be better than the real-time data. This dat
 is only really useful when multiple hits on the data are done within a few hours.")
 
 (defun get-rtd-data-path (station-id)
+  "Returns the path of the rtd cache for the given station id"
   (concatenate 'string (get-station-cache-path station-id) *rtd-path*))
 
 (defun ensure-rtd-data-path-exists (station-id)
+  "Returns the path of the rtd cache for the given station-id and creates the directory path if it doesn't exist yet"
   (let ((cache-path (get-rtd-data-path station-id)))
     (ensure-directories-exist cache-path)
     cache-path))
@@ -46,36 +48,47 @@ is only really useful when multiple hits on the data are done within a few hours
   "Path for historical data cache.")
 
 (defun this-year ()
+  "Returns the current year"
   (nth-value 5 (get-decoded-time)))
 
 (defun get-year (time)
+  "Returns the year of the given universal time"
   (nth-value 5 (decode-universal-time time)))
 
 (defun get-month (time)
+  "Returns the month of the given universal time"
   (values (nth-value 4 (decode-universal-time time))
           (local-time:format-timestring nil (local-time:universal-to-timestamp time) :format '(:short-month))))
 
 (defun get-hist-data-path-date (time)
+  "Gets the historical data \"time\" path for a given universal time. This path is valid for both the cache and the NOAA urls,
+and it is either the year if time is from a previous year, or the month name if time is from this year."
   (multiple-value-bind (s m h d MM YY)
       (decode-universal-time time)
     (if (= YY (this-year))
         (local-time:format-timestring nil (local-time:universal-to-timestamp time) :format '(:short-month))
         YY)))
 
-(defun get-hist-data-path (station-id &optional month-or-year)
+(defun get-hist-cache-path (station-id &optional month-or-year)
+  "Returns the historical data cache path for the given station id and month or year if one is given."
   (format nil "~A~A~A/" (get-station-cache-path station-id) *hist-path* month-or-year))
 
-(defun get-hist-data-path-from-time (station-id time)
-  (get-hist-data-path station-id (get-hist-data-path-date time)))
+(defun get-hist-cache-path-from-time (station-id time)
+  "Returns the historical data cache path for the given station id and universal time"
+  (get-hist-cache-path station-id (get-hist-cache-path-date time)))
 
 (defun ensure-hist-path-exists (station-id &optional time)
+  "Returns the historical data cache path for the given station id and universal time \(if one is given\) and creates the
+directory path if it does not exist yet."
   (let ((cache-path (if time
-                        (get-hist-data-path-from-time station-id time)
-                        (get-hist-data-path station-id))))
+                        (get-hist-cache-path-from-time station-id time)
+                        (get-hist-cache-path station-id))))
     (ensure-directories-exist cache-path)
     cache-path))
 
 (defun try-get-url (url)
+  "Attempts a request at the given http url and returns the request as a string if it's successful and nil if it gets a 404
+error. Other request errors are not handled yet."
   (handler-case
       (dex:get url)
     (dex:http-request-not-found () nil)))
@@ -110,6 +123,8 @@ is only really useful when multiple hits on the data are done within a few hours
 ;; ----------------------------------- Raw Data (in string format) -------------------------------
 
 (defstruct (raw-data (:constructor make-raw-data (c a1 a2 r1 r2)))
+  "Structures a set of raw data for the data parsers. Raw data comes as a set of text files, one for each parameter. Each
+parameter is stored in a stream."
   (c nil :type stream :read-only t)
   (a1 nil :type stream :read-only t)
   (a2 nil :type stream :read-only t)
@@ -117,6 +132,7 @@ is only really useful when multiple hits on the data are done within a few hours
   (r2 nil :type stream :read-only t))
 
 (defun raw-data-from-strings (c-data a1-data a2-data r1-data r2-data)
+  "Creates a raw-data object from a set of parameter strings, which is how they will be retrieved from the NOAA server."
   (make-raw-data
    (make-string-input-stream c-data)
    (make-string-input-stream a1-data)
@@ -125,6 +141,7 @@ is only really useful when multiple hits on the data are done within a few hours
    (make-string-input-stream r2-data)))
 
 (defun raw-data-from-paths (c-path a1-path a2-path r1-path r2-path)
+  "Creates a raw-data object from a set of parameter file paths, which is how they will be retrieved from the cache."
   (make-raw-data
    (open c-path)
    (open a1-path)
@@ -133,13 +150,16 @@ is only really useful when multiple hits on the data are done within a few hours
    (open r2-path)))
 
 (defun raw-data->list (rd)
+  "Returns the raw-data parameters as a list to make mapping with them easier"
   (list (raw-data-c rd) (raw-data-a1 rd) (raw-data-a2 rd) (raw-data-r1 rd) (raw-data-r2 rd)))
 
 (defun raw-data-close (rd)
+  "Closes all of the streams in a raw-data object"
   (mapc #'close (raw-data->list rd)))
 
 (defmacro with-open-rd-files ((rd c-path a1-path a2-path r1-path r2-path) &body body)
-  "Opens the real-time data files from the rtd cache for a station. These files will need to be manually closed."
+  "Opens the given raw data files, creates a raw-data object with them and places that into the rd symbol, then executes body
+before closing the files."
   `(handler-case
        (let ((,rd (raw-data-from-paths ,c-path ,a1-path ,a2-path ,r1-path ,r2-path)))
          (prog1
@@ -148,6 +168,7 @@ is only really useful when multiple hits on the data are done within a few hours
     (file-does-not-exist () nil)))
 
 (defun raw-data-reset (rd)
+  "Resets the streams in a raw-data object to the beginning."
   (mapc (lambda (getter)
           (file-position (funcall getter rd) 0))
         (list #'raw-data-c #'raw-data-a1 #'raw-data-a2 #'raw-data-r1 #'raw-data-r2)))
@@ -161,20 +182,23 @@ is only really useful when multiple hits on the data are done within a few hours
     (raw-data->list rd))))
 
 (defun eof-p (stream)
+  "Returns non-nil if the end of file has been reached on the given stream"
   (not (peek-char nil stream nil nil)))
 
 (defun raw-data-eof-p (rd)
+  "Returns non-nil if the end of file has been reached on any of the streams in the given raw-data object."
   (some #'eof-p (raw-data->list rd)))
 
 (defvar *date-scan* (ppcre:create-scanner "(\\d{4}) (\\d{2}) (\\d{2}) (\\d{2}) (\\d{2})")
   "Regex for retrieving the date from a buoy data row")
 
-(defun parse-time (row-str)
+(defun parse-time (line)
+  "Extracts the time from a given raw data line and returns it as a universal timestamp"
   (ppcre:register-groups-bind
-   ((#'parse-integer year month day hour min))
-   (*date-scan*
-    row-str)
-   (encode-universal-time 0 min hour day month year)))
+      ((#'parse-integer year month day hour min))
+      (*date-scan*
+       line)
+    (encode-universal-time 0 min hour day month year)))
 
 ;;; Real Time Data
 
@@ -184,8 +208,8 @@ is only really useful when multiple hits on the data are done within a few hours
 Each parameter comes in a separate file (which are provided to parse-rtd as strings). Each line of each file starts with
 a 5-number date, and the values of the parameter for each frequency are given as: <val1> (<freq1>) <val2> (<freq2>) ...
 The separation frequency is given in the spec file (which defines the 'c' parameter)."
-  (let ((fsep-scan (ppcre:create-scanner "([0-9.]+)"))                                     ; regex for getting the fsep
-        (stat-freq-scan (ppcre:create-scanner "([0-9.]+) \\(([0-9.]+)\\)")))           ; regex for getting the stat and freq
+  (let ((fsep-scan (ppcre:create-scanner "([0-9.]+)"))                        ; regex for getting the fsep
+        (stat-freq-scan (ppcre:create-scanner "([0-9.]+) \\(([0-9.]+)\\)")))  ; regex for getting the stat and freq
     (labels ((parse-line (line c-data?)
                "Parses a single line from one of the parameter files.
 
@@ -247,6 +271,7 @@ The separation frequency is given in the spec file (which defines the 'c' parame
       (parse-next-entry '()))))
 
 (defun get-station-rtd-cache-paths (station-id)
+  "Returns the real-time parameter cache paths for the given station id"
   (let ((cache-path (ensure-rtd-data-path-exists station-id)))
         (values
          (str:concat cache-path "c")
@@ -263,7 +288,9 @@ The separation frequency is given in the spec file (which defines the 'c' parame
         (raw-data->list rd)))
 
 (defun download-station-rtd (station-id &optional start-time end-time cache-data)
-  "Downloads the real-time data for the specified station ID"
+  "Downloads the real-time data for the specified station id, and returns it as a parsed set of spectral points. If
+start-time is non-nil, then only data points after that \(universal\) time will be returned. If end-time is non-nil then
+only data points before that time will be returned. If cache-data is non-nil, the raw data will be saved in the data cache."
   (flet ((get-data (path)
            (try-get-url (format nil "https://www.ndbc.noaa.gov/data/realtime2/~D.~A" station-id path))))
     (let ((c-data (get-data "data_spec"))
@@ -280,16 +307,11 @@ The separation frequency is given in the spec file (which defines the 'c' parame
 
       (parse-rtd (raw-data-from-strings c-data a1-data a2-data r1-data r2-data) start-time end-time))))
 
-;; (defun open-station-rtd-cache (station-id)
-;;   "Opens the real-time data files from the rtd cache for a station. These files will need to be manually closed. It's probably
-;; better to use with-open-rd-files if you can."
-;;   (handler-case
-;;       (multiple-value-bind (c a1 a2 r1 r2)
-;;           (get-station-rtd-cache-paths station-id)
-;;         (raw-data-from-paths c a1 a2 r1 r2))
-;;     (file-does-not-exist () nil)))
-
 (defun read-station-rtd-cache (station-id &optional start-time end-time)
+  "Reads real-time data from the rtd cache for the given station id. If start-time is non-nil, then only data points after
+that \(universal\) time will be returned. If end-time is non-nil then only data points before that time will be returned. 
+
+If the rtd cache for the given station does not exist, then the return value will be nil."
   (multiple-value-bind (c a1 a2 r1 r2)
       (get-station-rtd-cache-paths station-id)
     (with-open-rd-files (rd c a1 a2 r1 r2)
@@ -305,6 +327,8 @@ entry. Returns nil if data is at eof."
           (t nil))))                      ; Reached eof return nil
 
 (defun get-rtd-cache-time (station-id)
+  "Returns the latest time from the rtd cache for the given station id. If the cache does not exist then the return value
+will be nil."
   (multiple-value-bind (c a1 a2 r1 r2)
       (get-station-rtd-cache-paths station-id)
     (with-open-rd-files (rd c a1 a2 r1 r2)
@@ -318,9 +342,8 @@ entry. Returns nil if data is at eof."
 (defun parse-hist (rd &optional start-time end-time)
   "Parses historical data formatted strings into a list of spectral-points. 
 
-Each parameter comes in a separate file (which are provided to parse-rtd as strings). Each line of each file starts with
-a 5-number date, and the values of the parameter for each frequency are given as: <val1> <val2> ...
-The separation frequency is given in the spec file (which defines the 'c' parameter)."
+Each parameter comes in a separate stream. Each line of each stream starts with a 5-number date, followed by a series of
+space-separated floating point values. The header for each file contains the set of frequencies, one for each row of data."
   (let ((freqs))                                                ; register for frquencies 
     (labels ((parse-line (line)
                "Parses a single line from one of the parameter files."
@@ -380,6 +403,8 @@ The separation frequency is given in the spec file (which defines the 'c' parame
       (parse-next-entry '()))))
 
 (defun get-station-hist-cache-paths (station-id time)
+  "Returns the paths of the historical data cache files for the given station id, in which the given time is, or should be,
+stored."
   (let ((cache-path (ensure-hist-path-exists station-id time)))
         (values
          (str:concat cache-path "c")
@@ -423,14 +448,18 @@ The separation frequency is given in the spec file (which defines the 'c' parame
       station-id))))
 
 (defun download-station-hist (station-id start-time &optional end-time cache-data)
-  "Historical data is kept in yearly chunks for each year up to the current one. For the current year, data is kept in monthly
-chunks. The url format for each is slightly different, so we need to choose the url based on whether time is from this year
-\(at the time that this function is called\) or from a previous year.
+  "Downloads the historical data \(if it exists\) in which the given start time is stored for the given station id. Only data
+  points with times equal to or later than the start time will be returned and, if non-nil, only data points with times
+  before end-time will be returned.
 
-download-station-hist will download all data for the year/month of the start-time parameter, and it will parse the data until
-the end-time or the end of the data is reached.
+  Historical data is kept in yearly chunks for each year up to the current one. For the current year, data is kept in monthly
+  chunks. The url format for each is slightly different, so we need to choose the url based on whether time is from this year
+  \(at the time that this function is called\) or from a previous year.
 
-If cache-data is non-nil, the downloaded data will be cached locally for future use."
+  download-station-hist will download all data for the year/month of the start-time parameter, and it will parse the data
+  until the end-time or the end of the data is reached.
+
+  If cache-data is non-nil, the downloaded data will be cached locally for future use."
   (labels ((get-year-data (char-key path)
              "Gets data at a 'year' url"
              (try-get-url
@@ -464,12 +493,18 @@ If cache-data is non-nil, the downloaded data will be cached locally for future 
         (parse-hist (apply #'raw-data-from-strings data) start-time end-time)))))
 
 (defun read-station-hist-cache (station-id start-time &optional end-time)
+  "Reads the historical data from the cache \(if it exists\) in which the given start time is stored for the given station
+  id. Only data points with times equal to or later than the start time will be returned and, if non-nil, only data points
+  with times before end-time will be returned."
   (multiple-value-bind (c a1 a2 r1 r2)
       (get-station-hist-cache-paths station-id start-time)
     (with-open-rd-files (rd c a1 a2 r1 r2)
       (parse-hist rd start-time end-time))))
 
 (defun get-hist-data (station-id start-time end-time &optional cache-new-data)
+  "Returns all available historical data, preferably from the cache, if it's available there, between start-time and end-time
+for the given station id. If cache-new-data is non-nil, then any data not found in the cache, and retrieved from the web will
+be cached for later use."
   (labels ((get-data (start)
              (format t
                      "start: ~A~%"
@@ -486,9 +521,9 @@ If cache-data is non-nil, the downloaded data will be cached locally for future 
       ;; This will be the beginning of the next month in all cases
       (let* ((last-end (spectral-point-ts (car next-data))))
         (format t
-                     "last-end: ~A~%"
-                     (local-time:format-timestring nil (local-time:universal-to-timestamp last-end)
-                                                   :format local-time:+asctime-format+))
+                "last-end: ~A~%"
+                (local-time:format-timestring nil (local-time:universal-to-timestamp last-end)
+                                              :format local-time:+asctime-format+))
         (setq next-start
               (multiple-value-bind (s m h d mon yr) (decode-universal-time last-end)
                 (let* ((mon (1+ mon))
